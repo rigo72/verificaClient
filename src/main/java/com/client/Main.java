d5323dbdb585920be9802dd26c91dc43d3f31849
rigo72 <mattia.gori27@gmail.com>
2024-10-28 13:24:24 +0100

    Commit 2

--- src/main/java/com/client/Main.java
+++ src/main/java/com/client/Main.java
@@ -1,41 +1,42 @@
 package com.client;
+
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
-public class Main {
-    private static Scanner s = new Scanner(System.in);
+public class Main { 
     public static void main(String[] args) throws IOException{
-        System.out.println("Client pronto a collegarsi");
-        Socket myClientSocket = new Socket("localhost", 3000);
-        String clientChoice;
-        BufferedReader in = new BufferedReader(new InputStreamReader(myClientSocket.getInputStream()));
-        DataOutputStream out = new DataOutputStream(myClientSocket.getOutputStream());
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        while (true) {
-            System.out.println("Inserisci la nota da memorizzare o digita LISTA per vedere tutte le note");
-            input = scanner.nextLine();
-            if (input.equalsIgnoreCase("ESCI")) {
-                System.out.println("!");
-                break;
-            }else if(input.equalsIgnoreCase("LISTA")) {
-                System.out.println("?");
-                String serverResponse;
-                while (!(serverResponse = in.readLine()).equals("@")) {
-                    System.out.println(serverResponse);
+        try(Socket s = new Socket("localhost", 3000);
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            Scanner scanner = new Scanner(System.in)){
+                System.out.println("CONNESSO");
+                String input;
+                while (true) {
+                    System.out.println("Inserisci la nota da memorizzare o digita LISTA per vedere tutte le note\n");
+                    input = scanner.nextLine();
+                    if (input.equalsIgnoreCase("ESCI")) {
+                        out.println("!");
+                        break;
+                    }else if(input.equalsIgnoreCase("LISTA")) {
+                        out.println("?");
+                        String serverResponse;
+                        while (!(serverResponse = in.readLine()).equals("@")) {
+                            System.out.println(serverResponse);
+                        }
+                    }else{
+                        System.out.println(input);
+                        String response = in.readLine();
+                        if ("OK".equals(response)) {
+                            System.out.println("Nota salvata");
+                        }
+                    }
                 }
-            }else{
-                System.out.println(input);
-                String response = in.readLine();
-                if ("OK".equals(response)) {
-                    System.out.println("Nota salvata");
-                }
+            }catch(IOException e){
+                e.printStackTrace();
             }
-        }
-        s.close();
-        myClientSocket.close();
+            System.out.println("Disconnesso");
         }
 }
