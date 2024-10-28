34d96cace6422e5a6057cfd32c205f527b3b931c
rigo72 <mattia.gori27@gmail.com>
2024-10-28 12:17:46 +0100

    Commit 2

--- src/main/java/com/client/Main.java
+++ src/main/java/com/client/Main.java
@@ -1,50 +1,41 @@
 package com.client;
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Scanner;
-
 public class Main {
-
     private static Scanner s = new Scanner(System.in);
-
-    public static int showmenu() {
-        System.out.println("CLIENT");
-        System.out.println("Scegli cosa fare con la stringa da inviare");
-        System.out.println("1 - trasfomare in MAIUSCOLO");
-        System.out.println("2 - trasfomare in minuscol");
-        System.out.println("3 - ribaltare i caratteri");
-        System.out.println("4 - contare i caratteri\n");
-        System.out.println("ALTRO - USCITA");
-
-        System.out.println("Inserire la scelta col numero corrispondente");
-
-        int choice = s.nextInt();
-        s.nextLine();
-        return choice;
-    }
-
     public static void main(String[] args) throws IOException{
         System.out.println("Client pronto a collegarsi");
-        Socket myClientSocket = new Socket("172.21.105.88", 3645);
-        int clientChoice;
+        Socket myClientSocket = new Socket("localhost", 3000);
+        String clientChoice;
         BufferedReader in = new BufferedReader(new InputStreamReader(myClientSocket.getInputStream()));
         DataOutputStream out = new DataOutputStream(myClientSocket.getOutputStream());
-        do {
-            clientChoice = showmenu();
-            System.out.println("Inserire la stringa da inviare: ");
-            String sending_string = s.nextLine();
-            out.writeBytes(sending_string + '\n');
-            out.writeBytes(String.valueOf(clientChoice));
-            String edited_string = in.readLine();
-            System.out.println("stringa modificata dal server: " + edited_string);
-        } while(clientChoice >= 1 || clientChoice <= 4); 
-
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (true) {
+            System.out.println("Inserisci la nota da memorizzare o digita LISTA per vedere tutte le note");
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("ESCI")) {
+                System.out.println("!");
+                break;
+            }else if(input.equalsIgnoreCase("LISTA")) {
+                System.out.println("?");
+                String serverResponse;
+                while (!(serverResponse = in.readLine()).equals("@")) {
+                    System.out.println(serverResponse);
+                }
+            }else{
+                System.out.println(input);
+                String response = in.readLine();
+                if ("OK".equals(response)) {
+                    System.out.println("Nota salvata");
+                }
+            }
+        }
         s.close();
         myClientSocket.close();
-        
         }
 }
